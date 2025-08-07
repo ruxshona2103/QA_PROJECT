ba171e5be5372995c4ac60717c2127c6e9943b77
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-07 11:07:00 +0500

    conftest updated 3

--- conftest.py
+++ conftest.py
@@ -11,14 +11,21 @@ from pages.text_box_page import TextBoxPage
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--start-maximized")
-    options.add_argument("--force-device-scale-factor=0.60")
-    user_data_dir = tempfile.mkdtemp()
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    user_data_dir = tempfile.mkdtemp(prefix="chrome-user-data-")
     options.add_argument(f"--user-data-dir={user_data_dir}")
+
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
+
 
 
 @pytest.fixture
