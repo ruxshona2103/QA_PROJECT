aa0320ba3527782dcde462b7afa2142c4faccb69
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-07 14:13:45 +0500

    conftest updated 4

--- conftest.py
+++ conftest.py
@@ -7,7 +7,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_page import LoginPage
 from pages.text_box_page import TextBoxPage
 
-
 @pytest.fixture
 def driver():
     options = Options()
@@ -18,11 +17,15 @@ def driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.5735.90 Safari/537.36"
+    )
     user_data_dir = tempfile.mkdtemp(prefix="chrome-user-data-")
     options.add_argument(f"--user-data-dir={user_data_dir}")
-
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+    driver.set_page_load_timeout(30)
+
     yield driver
     driver.quit()
 
