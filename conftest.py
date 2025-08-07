d56c456accef22304e18ea38d1aa7a0e3314579a
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-07 10:46:53 +0500

    conftest updated 2

--- conftest.py
+++ conftest.py
@@ -1,21 +1,20 @@
-import tempfile
 import pytest
+import tempfile
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_page import LoginPage
 from pages.text_box_page import TextBoxPage
+
 
 @pytest.fixture
 def driver():
     options = Options()
     options.add_argument("--start-maximized")
     options.add_argument("--force-device-scale-factor=0.60")
-
     user_data_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={user_data_dir}")
-
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
